c376d4e1b1e3c7c75ceb436addef34760ffe6ca9
SBGoods <github@simplebox.anonaddy.com>
2022-10-13 13:22:56 -0400

    Remove unnecessary tests

--- internal/provider/data_source_test.go
+++ internal/provider/data_source_test.go
@@ -38,66 +38,6 @@ func TestAccDataSource_inputs(t *testing.T) {
 	})
 }
 
-func TestAccTimeOffset_Upgrade_Basic(t *testing.T) {
-	dsn := "data.null_data_source.test"
-
-	resource.Test(t, resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: providerVersion311(),
-				Config:            testAccDataSourceConfig_basic,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(dsn, "random"),
-					resource.TestCheckResourceAttr(dsn, "has_computed_default", "default"),
-				),
-			},
-			{
-				ProtoV5ProviderFactories: protoV5ProviderFactories(),
-				Config:                   testAccDataSourceConfig_basic,
-				PlanOnly:                 true,
-			},
-			{
-				ProtoV5ProviderFactories: protoV5ProviderFactories(),
-				Config:                   testAccDataSourceConfig_basic,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(dsn, "random"),
-					resource.TestCheckResourceAttr(dsn, "has_computed_default", "default"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccTimeOffset_Upgrade_Inputs(t *testing.T) {
-	dsn := "data.null_data_source.test"
-
-	resource.Test(t, resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: providerVersion311(),
-				Config:            testAccDataSourceConfig_inputs,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(dsn, "random"),
-					resource.TestCheckResourceAttr(dsn, "outputs.foo", "bar"),
-				),
-			},
-			{
-				ProtoV5ProviderFactories: protoV5ProviderFactories(),
-				Config:                   testAccDataSourceConfig_inputs,
-				PlanOnly:                 true,
-			},
-			{
-				ProtoV5ProviderFactories: protoV5ProviderFactories(),
-				Config:                   testAccDataSourceConfig_inputs,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(dsn, "random"),
-					resource.TestCheckResourceAttr(dsn, "outputs.foo", "bar"),
-				),
-			},
-		},
-	})
-}
-
 const testAccDataSourceConfig_basic = `
 data "null_data_source" "test" {
 }
