63dd2fa20066cdc59792c17a5134d79c1bd35308
SBGoods <github@simplebox.anonaddy.com>
2022-10-13 13:18:01 -0400

    Refactor data_source.tf example to be more readable

--- examples/data-sources/null_data_source/data-source.tf
+++ examples/data-sources/null_data_source/data-source.tf
@@ -1,9 +1,13 @@
 data "null_data_source" "values" {
   inputs = {
-    all_server_ids = concat(aws_instance.green.*.id,
-    aws_instance.blue.*.id)
-    all_server_ips = concat(aws_instance.green.*.private_ip,
-    aws_instance.blue.*.private_ip)
+    all_server_ids = concat(
+      aws_instance.green.*.id,
+      aws_instance.blue.*.id,
+    )
+    all_server_ips = concat(
+      aws_instance.green.*.private_ip,
+      aws_instance.blue.*.private_ip,
+    )
   }
 }
 
